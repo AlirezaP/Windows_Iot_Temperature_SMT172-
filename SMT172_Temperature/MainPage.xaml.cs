3bf0dfb4b4ad50ae414f408ed99968f7d64d98a4
Alireza <alireza_paridar@outlook.com>
2017-03-09 19:27:29 +0330

    Add project files.

---
+++ SMT172_Temperature/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SMT172_Temperature
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        int outputPin = 2;
+        SMT172 smt;
+        private void Grid_Loading(FrameworkElement sender, object args)
+        {
+
+            listView.ShowsScrollingPlaceholders = true;
+
+            smt = new SMT172(outputPin);
+
+            var timer2 = new DispatcherTimer();
+            timer2.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer2.Tick += Timer2_Tick; ;
+
+            timer2.Start();
+        }
+
+        private async void Timer2_Tick(object sender, object e)
+        {
+            var t = await smt.ReadTemperature();
+            txtTemperature.Text = $"{t.ToString("00")} °C";
+            txtDateTime.Text = DateTime.Now.ToString("hh:mm:ss");
+            var res = $"T:{t.ToString()} at {DateTime.Now.ToString("hh/mm/ss")}";
+
+            listView.Items.Add($"T:{res.ToString()} at {DateTime.Now.ToString("hh/mm/ss")}");
+
+            if (listView.Items.Count() > 1)
+            {
+                listView.SelectedIndex = listView.Items.Count() - 1;
+                listView.ScrollIntoView(listView.Items[listView.Items.Count() - 1]);
+            }
+
+            if (listView.Items.Count >= 100)
+            {
+                listView.Items.Clear();
+            }
+        }
+
+    }
+}
---
+++ SMT172_Temperature/SMT172.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace SMT172_Temperature
+{
+    class SMT172
+    {
+
+        private double pwmh = 0;
+        private double pwml = 0;
+
+        private int _pin = 2;
+        private GpioController gpio = null;
+        private GpioPin tPintObj = null;
+
+        public SMT172(int pin)
+        {
+            _pin = pin;
+
+            if (gpio == null)
+            {
+                gpio = GpioController.GetDefault();
+            }
+
+            if (tPintObj == null)
+            {
+                tPintObj = gpio.OpenPin(_pin);
+                tPintObj.SetDriveMode(GpioPinDriveMode.Input);
+            }
+
+        }
+
+        public async Task<double> ReadTemperature()
+        {
+            pwmh = 0;
+            pwml = 0;
+
+            double sum = 0;
+
+            for (int avg = 0; avg <= 7; avg++)
+            {
+                for (int i = 0; i < 2000; i++)
+                {
+                    if (tPintObj.Read() == GpioPinValue.High)
+                    {
+                        pwmh++;
+                    }
+                    else
+                    {
+                        pwml++;
+                    }
+
+                    //delay
+                    //long microseconds = 1;
+                    //var ticks = (TimeSpan.TicksPerMillisecond / 1000) * microseconds;
+
+                    await Task.Delay(TimeSpan.FromTicks(10));
+                }
+
+                sum += ((pwmh / (pwml + pwmh)));
+            }
+
+            //calc
+            //return ((pwmh/(pwml+pwmh))-0.32)/ 0.0047;
+
+            double div = (double)sum / (double)8;
+            double temperature = (double)212.77 * div - (double)68.085;
+            return temperature;
+        }
+    }
+}
